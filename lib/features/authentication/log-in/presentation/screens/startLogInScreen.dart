d31d926f56344a8aadf6f6efe78468c3a802ed59
George Sladkovsky <102670724+GeorgiyGit@users.noreply.github.com>
2025-02-07 01:11:15 +0100

    fix: fix saving of token [MTAA-7]

--- lib/features/authentication/log-in/presentation/screens/startLogInScreen.dart
+++ lib/features/authentication/log-in/presentation/screens/startLogInScreen.dart
@@ -44,92 +44,95 @@ class _StartLogInState extends State<StartLogInScreen> {
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-      appBar: AppBar(
-        actions: <Widget>[
-          Container(
-              margin: const EdgeInsets.fromLTRB(0, 0, 21, 0),
-              child: IconButton(
-                icon: const Icon(Icons.dark_mode),
-                tooltip: 'Change theme',
-                onPressed: () {
-                  context.read<ThemeBloc>().add(ToggleThemeEvent());
-                },
-              ))
-        ],
-      ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(28),
-        child: Center(child: Column(
-          children: [
-            Text(
-              'Log In',
-              textAlign: TextAlign.center,
-              style: Theme.of(context).textTheme.headlineLarge,
-            ),
-            const SizedBox(height: 18),
-            StartLogInForm(
-                formKey: _formKey,
-                loginController: loginController,
-                passwordController: passwordController),
-            const SizedBox(height: 19),
-            isLoading
-                ? const DotLoader():
-                BlocBuilder<VerificationEmailPhoneBloc,
-                                VerificationEmailPhoneState>(
-                            builder: (context, state) {
-                          return TextButton(
-                    onPressed: () async {
-                      if (_formKey.currentState!.validate()) {
-                        setState(() => isLoading = true);
-
-                        String input = state.str.trim();
-                        String? email;
-                        String? phone;
-
-                        // Check if input is an email or phone number
-                        if (RegExp(r'^[^@]+@[^@]+\.[^@]+').hasMatch(input)) {
-                          email = input; // It's an email
-                        } else if (RegExp(r'^\+?\d+$').hasMatch(input)) {
-                          phone = input; // It's a phone number
-                        }
-
-                        Token? res = await widget.identityApi.logIn(
-                          LogInRequest(email: email, phone: phone, password: passwordController.text),
-                        );
-
-                        setState(() => isLoading = false);
-
-                        if (res != null) {
-                          if (email != null) {
-                            TokenStorage.saveToken("email_token_${res.token}");
-                          } else if (phone != null) {
-                            TokenStorage.saveToken("phone_token_${res.token}");
-                          }
-                        _navigateToChangeFullNameScreen();
-                        }
-                      }
-                    },
-                    style: Theme.of(context).textButtonTheme.style,
-                    child: Text(
-                      'Start',
-                    ),);},
-                  ),
-            const SizedBox(height: 8),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text("Need an account? "),
-                              Text(
-                                  "Sign Up",
-                                  style: Theme.of(context).textTheme.labelSmall,
-                                ),
-                            ],
-                          ),
+        appBar: AppBar(
+          actions: <Widget>[
+            Container(
+                margin: const EdgeInsets.fromLTRB(0, 0, 21, 0),
+                child: IconButton(
+                  icon: const Icon(Icons.dark_mode),
+                  tooltip: 'Change theme',
+                  onPressed: () {
+                    context.read<ThemeBloc>().add(ToggleThemeEvent());
+                  },
+                ))
           ],
         ),
-      ))
-    );
+        body: SingleChildScrollView(
+            padding: const EdgeInsets.all(28),
+            child: Center(
+              child: Column(
+                children: [
+                  Text(
+                    'Log In',
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.headlineLarge,
+                  ),
+                  const SizedBox(height: 18),
+                  StartLogInForm(
+                      formKey: _formKey,
+                      loginController: loginController,
+                      passwordController: passwordController),
+                  const SizedBox(height: 19),
+                  isLoading
+                      ? const DotLoader()
+                      : BlocBuilder<VerificationEmailPhoneBloc,
+                          VerificationEmailPhoneState>(
+                          builder: (context, state) {
+                            return TextButton(
+                              onPressed: () async {
+                                if (_formKey.currentState!.validate()) {
+                                  setState(() => isLoading = true);
+
+                                  String input = state.str.trim();
+                                  String? email;
+                                  String? phone;
+
+                                  // Check if input is an email or phone number
+                                  if (RegExp(r'^[^@]+@[^@]+\.[^@]+')
+                                      .hasMatch(input)) {
+                                    email = input; // It's an email
+                                  } else if (RegExp(r'^\+?\d+$')
+                                      .hasMatch(input)) {
+                                    phone = input; // It's a phone number
+                                  }
+
+                                  Token? res = await widget.identityApi.logIn(
+                                    LogInRequest(
+                                        email: email,
+                                        phone: phone,
+                                        password: passwordController.text),
+                                  );
+
+                                  setState(() => isLoading = false);
+
+                                  if (res != null) {
+                                    TokenStorage.saveToken(res.token);
+                                    TokenStorage.saveToken(res.token);
+                                    _navigateToChangeFullNameScreen();
+                                  }
+                                }
+                              },
+                              style: Theme.of(context).textButtonTheme.style,
+                              child: Text(
+                                'Start',
+                              ),
+                            );
+                          },
+                        ),
+                  const SizedBox(height: 8),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("Need an account? "),
+                      Text(
+                        "Sign Up",
+                        style: Theme.of(context).textTheme.labelSmall,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            )));
   }
 }
